6737a186da442c720a569e2662bc1bcc7b15bafd
Stephen Martinez <enharmonicnuance@gmail.com>
2019-10-05 19:22:59 -0700

    Excluded some files

--- Package.swift
+++ Package.swift
@@ -23,6 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ConVerto",
-            dependencies: []),
+            dependencies: [],
+            exclude: [
+                "ConVerto",
+                "ConVerto.podspec"
+            ]
+        ),
     ]
 )
